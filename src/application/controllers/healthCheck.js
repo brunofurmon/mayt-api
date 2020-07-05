7005dcb3c5323aa3da1c1850b28677fa89ba2d2d
Bruno Furtado <brunofurmon@gmail.com>
2020-07-04 21:07:23 -0300

    Initial webserver

---
+++ src/application/controllers/healthCheck.js
@@ -0,0 +1,16 @@
+const { OK, INTERNAL_SERVER_ERROR } = require('http-status');
+
+module.exports = () => {
+    const healthCheck = async ctx => {
+
+        const everythingIsOk = true;
+
+        const status = everythingIsOk? OK: INTERNAL_SERVER_ERROR;
+        const body = everythingIsOk? "OK": "KO";;
+
+        ctx.status = status;
+        ctx.body = body
+    };
+
+    return { healthCheck };
+};
